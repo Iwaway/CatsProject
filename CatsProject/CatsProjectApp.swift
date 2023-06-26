747c50179b6f712c46afaa906ee602e869480946
Iwaway <belokonegor2000@gmail.com>
2023-06-26 21:58:50 +0300

    Initial Commit

---
+++ CatsProject/CatsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsProjectApp.swift
+//  CatsProject
+//
+//  Created by Yehor Belokon on 26.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CatsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
