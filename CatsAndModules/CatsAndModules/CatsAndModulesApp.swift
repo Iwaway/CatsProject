35fecbb6fe6a39a6340d76b50c2fc0600e64b4ac
Iwaway <belokonegor2000@gmail.com>
2023-06-28 11:40:55 +0300

    version up

---
+++ CatsAndModules/CatsAndModules/CatsAndModulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAndModulesApp.swift
+//  CatsAndModules
+//
+//  Created by Yehor Belokon on 28.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CatsAndModulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
